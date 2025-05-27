217f2233d976d0a72a35814bba916945a1c46c6a
RagahTzy <ragahediting@gmail.com>
2025-05-27 09:29:03 +0700

    hari kedua belajar python

--- pertama.py
+++ pertama.py
@@ -20,16 +20,41 @@
 # print(f"Nama lengkap anda adalah: {fname} {lname}")
 
 #if statement
-score = int(input("masukkan score anda: "))
-if score >= 90:
-    print("anda memiliki akreditasi A")
-elif score >= 80 and score <90:
-    print("anda memiliki akreditasi B")
-elif score >= 70 and score <80:
-    print("anda memiliki akreditasi C")
-elif score >= 60 and score <70:
-    print("anda memiliki akreditasi D")
-elif score >= 0 and score <60:
-    print("anda memiliki akreditasi E")
-else:
-    print("score tidak boleh minus")
+# score = int(input("masukkan score anda: "))
+# if score >= 90:
+#     print("anda memiliki akreditasi A")
+# elif score >= 80 and score <90:
+#     print("anda memiliki akreditasi B")
+# elif score >= 70 and score <80:
+#     print("anda memiliki akreditasi C")
+# elif score >= 60 and score <70:
+#     print("anda memiliki akreditasi D")
+# elif score >= 0 and score <60:
+#     print("anda memiliki akreditasi E")
+# else:
+#     print("score tidak boleh minus")
+
+#for loop
+# bapak = ["Kandar", "Lukman", "Rahmat", "Helmi", "Khairi"]
+#
+# for nama in bapak:
+#     print("nama bapak anak MI: " + nama)
+
+# for num in range(0,11,2):
+#     print(num)
+
+#function
+angka = int(input("Masukkan angka : "))
+def lukman(x):
+    for num in range(1,100):
+        if num % 3 == 0 and num % 5 == 0:
+            print("Lukman")
+        elif num % 3 == 0:
+            print("Luk")
+        elif num % 5 == 0:
+            print("man")
+        else:
+            print(num)
+
+print("bersiaplah akan muncul lukman:")
+lukman(angka)
